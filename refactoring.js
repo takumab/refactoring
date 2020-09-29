1f60cf1ec68c5f568adb440cc7fafed5318dea09
takumab <takumab@gmail.com>
2020-09-29 19:52:21 +0100

    Update to correct param

--- refactoring.js
+++ refactoring.js
@@ -30,10 +30,12 @@ function statement(invoice, plays) {
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" === play.type) {
+            volumeCredits += Math.floor(perf.audience / 5);
+        }
 
         // print line for this order
-        result += `${play.name}: ${format(totalAmount / 100)} (${perf.audience} seats)\n`;
+        result += `${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
