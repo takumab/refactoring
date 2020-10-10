dca53207366a1707c061c615eb1d3bec2dc5965a
takumab <takumab@gmail.com>
2020-10-10 18:39:32 +0100

    Refactor out switch statement

--- refactoring.js
+++ refactoring.js
@@ -1,5 +1,26 @@
-const [invoice] = require('./invoices.json');
-const plays = require('./plays.json');
+
+
+function amountFor(performance, play) {
+    let thisAmount = 0;
+    switch (play.type) {
+        case "tragedy":
+            thisAmount = 40000;
+            if (performance.audience > 30) {
+                thisAmount += 1000 * (performance.audience - 30);
+            }
+            break;
+        case "comedy":
+            thisAmount = 30000;
+            if (performance.audience > 20) {
+                thisAmount += 10000 + 500 * (performance.audience - 20);
+            }
+            thisAmount += 300 * performance.audience;
+            break;
+        default: throw new Error(`Unknown type: ${play.type}`);
+    }
+    return thisAmount;
+}
+
 
 function statement(invoice, plays) {
     let totalAmount = 0;
@@ -7,35 +28,19 @@ function statement(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     const format = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", maximumFractionDigits: 2}).format;
 
-    for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        let thisAmount = 0;
+    for (let performance of invoice.performances) {
+        const play = plays[performance.playID];
+        let thisAmount = amountFor(performance, play);
 
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default: throw new Error(`Unknown type: ${play.type}`);
-        }
         // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
+        volumeCredits += Math.max(performance.audience - 30, 0);
         // add extra credit for every ten comedy attendees
         if ("comedy" === play.type) {
-            volumeCredits += Math.floor(perf.audience / 5);
+            volumeCredits += Math.floor(performance.audience / 5);
         }
 
         // print line for this order
-        result += `${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+        result += `${play.name}: ${format(thisAmount / 100)} (${performance.audience} seats)\n`;
         totalAmount += thisAmount;
     }
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
