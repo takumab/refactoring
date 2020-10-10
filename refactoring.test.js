512954a123be42bb95d9ab9275523f8ae00c7be7
takumab <takumab@gmail.com>
2020-10-10 15:14:38 +0100

    Add fake data to tests as opposed to importing form json files. I imagine those as being the DBs and it's not wise to connect to DB in unit tests.

--- refactoring.test.js
+++ refactoring.test.js
@@ -1,17 +1,53 @@
-const [invoice] = require('./invoices.json');
-const plays = require('./plays.json');
 const statement = require('./refactoring');
 
+
+
 describe('Refactoring', () => {
-    const expected = `Statement for BigCo
-Hamlet: $650.00 (55 seats)
-As You Like It: $580.00 (35 seats)
-Othello: $500.00 (40 seats)
-Amount owed is $1,730.00
-You earned 47 credits
-`
+    const [plays] =  [
+       {
+           "hamlet": {
+               "name": "Hamlet",
+               "type": "tragedy"
+           },
+           "as-like": {
+               "name": "As You Like It",
+               "type": "comedy"
+           },
+           "othello": {
+               "name": "Othello",
+               "type": "tragedy"
+           }
+       }
+   ]
+
+    const [invoice] =  [
+        {
+            "customer": "Customer 1",
+            "performances": [
+                {
+                    "playID": "hamlet",
+                    "audience": 55
+                },
+                {
+                    "playID": "as-like",
+                    "audience": 35
+                },
+                {
+                    "playID": "othello",
+                    "audience": 40
+                }
+            ]
+        }
+    ]
+
+
+
     it('should return order statement', function () {
+        // arrange
+        const expected = `Statement for Customer 1\nHamlet: $650.00 (55 seats)\nAs You Like It: $580.00 (35 seats)\nOthello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits\n`
+        // act
         const actual = statement(invoice, plays);
+        // assert
         expect(actual).toBe(expected);
     });
 });
