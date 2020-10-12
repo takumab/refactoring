fa18fd2213877dc6ddcb32a523021feb02ada95b
takumab <takumab@gmail.com>
2020-10-12 21:41:48 +0100

    Refactor temp variable

--- refactoring.js
+++ refactoring.js
@@ -1,5 +1,35 @@
 
 
+
+function statement(invoice, plays) {
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = `Statement for ${invoice.customer}\n`;
+    const format = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", maximumFractionDigits: 2}).format;
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
+    for (let performance of invoice.performances) {
+        const play = playFor(performance);
+        let thisAmount = amountFor(performance, play);
+
+        // add volume credits
+        volumeCredits += Math.max(performance.audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        if ("comedy" === play.type) {
+            volumeCredits += Math.floor(performance.audience / 5);
+        }
+
+        // print line for this order
+        result += `${play.name}: ${format(thisAmount / 100)} (${performance.audience} seats)\n`;
+        totalAmount += thisAmount;
+    }
+    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `You earned ${volumeCredits} credits\n`;
+    return result;
+}
+
 function amountFor(aPerformance, play) {
     let result = 0;
     switch (play.type) {
@@ -21,31 +51,5 @@ function amountFor(aPerformance, play) {
     return result;
 }
 
-
-function statement(invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", maximumFractionDigits: 2}).format;
-
-    for (let performance of invoice.performances) {
-        const play = plays[performance.playID];
-        let thisAmount = amountFor(performance, play);
-
-        // add volume credits
-        volumeCredits += Math.max(performance.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) {
-            volumeCredits += Math.floor(performance.audience / 5);
-        }
-
-        // print line for this order
-        result += `${play.name}: ${format(thisAmount / 100)} (${performance.audience} seats)\n`;
-        totalAmount += thisAmount;
-    }
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
-    return result;
-}
 
 module.exports = statement;
