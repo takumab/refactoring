15f9082724723a3bcafe3ff9a8daf16d15306370
takumab <takumab@gmail.com>
2020-10-12 20:20:40 +0100

    Refactor param name to "aPerformance"

--- refactoring.js
+++ refactoring.js
@@ -1,20 +1,20 @@
 
 
-function amountFor(performance, play) {
+function amountFor(aPerformance, play) {
     let result = 0;
     switch (play.type) {
         case "tragedy":
             result = 40000;
-            if (performance.audience > 30) {
-                result += 1000 * (performance.audience - 30);
+            if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
             }
             break;
         case "comedy":
             result = 30000;
-            if (performance.audience > 20) {
-                result += 10000 + 500 * (performance.audience - 20);
+            if (aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
             }
-            result += 300 * performance.audience;
+            result += 300 * aPerformance.audience;
             break;
         default: throw new Error(`Unknown type: ${play.type}`);
     }
