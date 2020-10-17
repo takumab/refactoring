3d6182405fb3bd31b55b635b9311aa04613c8608
takumab <takumab@gmail.com>
2020-10-17 07:08:40 +0100

    Remove `play` variable

--- refactoring.js
+++ refactoring.js
@@ -10,9 +10,9 @@ function statement(invoice, plays) {
         return plays[aPerformance.playID];
     }
 
-    function amountFor(aPerformance, play) {
+    function amountFor(aPerformance) {
         let result = 0;
-        switch (play.type) {
+        switch (playFor(aPerformance).type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -26,24 +26,23 @@ function statement(invoice, plays) {
                 }
                 result += 300 * aPerformance.audience;
                 break;
-            default: throw new Error(`Unknown type: ${play.type}`);
+            default: throw new Error(`Unknown type: ${playFor(aPerformance).type}`);
         }
         return result;
     }
 
     for (let performance of invoice.performances) {
-        const play = playFor(performance);
-        let thisAmount = amountFor(performance, play);
+        let thisAmount = amountFor(performance);
 
         // add volume credits
         volumeCredits += Math.max(performance.audience - 30, 0);
         // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) {
+        if ("comedy" === playFor(performance).type) {
             volumeCredits += Math.floor(performance.audience / 5);
         }
 
         // print line for this order
-        result += `${play.name}: ${format(thisAmount / 100)} (${performance.audience} seats)\n`;
+        result += `${playFor(performance).name}: ${format(thisAmount / 100)} (${performance.audience} seats)\n`;
         totalAmount += thisAmount;
     }
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
